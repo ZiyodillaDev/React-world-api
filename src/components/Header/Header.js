8f5b8a3cc654692d407e930efd0d50d5aff4dcdd
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-01-31 19:24:05 +0500

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
+import { useRef } from "react";
+
 export const Header = () => {
+  const headerbg = document.querySelector(".hBg")
+  const modeBtn = document.querySelector(".modeBtn")
+  const handleDarkMode =()=>{
+    document.body.classList.toggle("mode")
+    headerbg.classList.toggle("h-mode")
+    if(document.body.classList =="mode"){
+      modeBtn.textContent = "🌙"
+    }else{
+          modeBtn.textContent = "🌞"
+    }
+
+  }
   return (
-    <header className="py-4 shadow">
+    <header className="py-4 shadow hBg">
       <div className="container">
         <div className="d-flex align-items-center">
           <h2 className="h2 fw-bold">Where in the world?</h2>
-          <button className="btn p-0 ms-auto fs-3">
+          <button className="btn p-0 ms-auto fs-3 modeBtn" onClick={handleDarkMode}>
             🌞
           </button>
         </div>
